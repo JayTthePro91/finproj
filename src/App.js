416a9e8002af77e8c7db5efb383b21ea658162e8
abdel <aboualyet63@gmail.com>
2020-12-13 08:34:47 -0500

    created forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+ 
+ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <form>
+    //   <input type ='text' placeholder='Search Food'></input>
+    //   {/* <input type="Submit"> Submit</input> */}
+    //   <button type="submit">Submit</button>
+    // </form>
+    <div>
+        <h1> Welcome to Our Food site </h1>
+        <form  className = 'forms'>
+          <input type="text" placeholder="Search Food" ></input>
+          <button type="submit">Submit</button>
+      </form>
     </div>
-  );
+    
+
+   );
 }
 
 export default App;
+
+
+
+
+
+
+
