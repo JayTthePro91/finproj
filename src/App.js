5339f571c7f27bb4d96f4222d66b192e839a7ebf
abdel <aboualyet63@gmail.com>
2020-12-14 07:04:07 -0500

    fetching and design Api

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
- 
- import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    // <form>
-    //   <input type ='text' placeholder='Search Food'></input>
-    //   {/* <input type="Submit"> Submit</input> */}
-    //   <button type="submit">Submit</button>
-    // </form>
-    <div>
-        <h1> Welcome to Our Food site </h1>
-        <form  className = 'forms'>
-          <input type="text" placeholder="Search Food" ></input>
-          <button type="submit">Submit</button>
-      </form>
-    </div>
-    
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      data: [],
+    };
+  }
 
-   );
+  onInputChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  getFood = (e) => {
+    e.preventDefault();
+    const { searchTerm } = this.state;
+
+    fetch(
+      `https://api.edamam.com/search?q=${searchTerm}&app_id=755850ce&app_key=b65ad11cb1529cc777336fdabaedde35`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data: data.hits });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1> Welcome to Our Food site </h1>
+          <form className="forms" onSubmit={this.getFood}>
+            <input
+              onChange={this.onInputChange}
+              type="text"
+              placeholder="Search Food"
+            ></input>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <ul class='list'>
+          {this.state.data.map((food) => {
+            return (
+              <li>
+                <p>{food.recipe.label}</p>
+                <img src={food.recipe.image} />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
-
-
-
-
-
-
-
