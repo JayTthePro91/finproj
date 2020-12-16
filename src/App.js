b195968ab4096c250257f70a076d9a5246b53ccc
abdel <aboualyet63@gmail.com>
2020-12-16 17:52:38 -0500

    modified app.js and styled app.css

--- src/App.js
+++ src/App.js
@@ -1,112 +1,59 @@
-
-import React,{useState} from "react";
+import React from "react";
 import "./App.css";
-import Axios from 'axios';
-import './App.css';
 
-
- 
- import './App.css';
-
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       searchTerm: "",
-//       data: [],
-//     };
-//   }
-
-//   onInputChange = (event) => {
-//     this.setState({ searchTerm: event.target.value });
-//   };
-
-//   getFood = (e) => {
-//     e.preventDefault();
-//     const { searchTerm } = this.state;
-
-//     fetch(
-//       `https://api.edamam.com/search?q=${searchTerm}&app_id=755850ce&app_key=b65ad11cb1529cc777336fdabaedde35`
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         this.setState({ data: data.hits });
-//       });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <div>
-//           <h1> Welcome to Our Food site </h1>
-//           <form className="forms" onSubmit={this.getFood}>
-//             <input
-//               onChange={this.onInputChange}
-//               type="text"
-//               placeholder="Search Food"
-//             ></input>
-//             <button type="submit">Submit</button>
-//           </form>
-//         </div>
-//         <ul class='list'>
-//           {this.state.data.map((food) => {
-//             return (
-//               <li>
-//                 <p>{food.recipe.label}</p>
-//                 <img src={food.recipe.image} />
-//               </li>
-//             );
-//           })}
-//         </ul>
-//       </div>
-//     );
-//   }
-
-
-
-
-
-const App = () =>{
-//   function App(){
-
-   const[thequery, thesetquery] = useState("")
-
-   const The_App_Id = "30c41784";
-
-   const The_App_Key = "a13a30df2bd96915e3a56b49d565ba03"
-
-  const url = `https://api.edamam.com/search?q=${thequery}&app_id=${The_App_Id}&app_key=${The_App_Key}`
-
-  const getData = async () =>{
-    const thesolution = await Axios.get(url);
-
-    console.log(thesolution);
-
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: "",
+      data: [],
+    };
   }
 
-  const onChange = (j) =>{
-    thesetquery(j.target.value)
+  onInputChange = (event) => {
+    this.setState({ searchTerm: event.target.value });
+  };
+
+  getFood = (e) => {
+    e.preventDefault();
+    const { searchTerm } = this.state;
+
+    fetch(
+      `https://api.edamam.com/search?q=${searchTerm}&app_id=755850ce&app_key=b65ad11cb1529cc777336fdabaedde35`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ data: data.hits });
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1> Welcome to Our Food site </h1>
+          <form className="forms" onSubmit={this.getFood}>
+            <input
+              onChange={this.onInputChange}
+              type="text"
+              placeholder="Search Food"
+            ></input>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+        <ul class='list'>
+          {this.state.data.map((food) => {
+            return (
+              <li>
+                <p>{food.recipe.label}</p>
+                <img src={food.recipe.image} alt='' />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
-
-
-  const onSubmit = (j) => {
-   j.preventDefault();
-   getData();
-  }
-  
-  return (
-
-    <div className = "App">
-    <h1> Welcome to our food site </h1>
-    <form className = 'forms' onSubmit = {onSubmit}>
-     <input type="text" placeholder="Search Food" autoComplete = "off" onChange={onChange} ></input>
-     <button type="submit">Submit</button>
-    </form>
-   
-    </div>
-
-   );
-
 }
+
 export default App;
